f63906b99effaff9f48046ba4296c950d4ea941b
rewove <rewove@github.com>
2019-07-12 00:03:45 +0100

    add own define ER network generater

--- create_network.R
+++ create_network.R
@@ -4,10 +4,33 @@ library(igraph)
 # basic paramters
 AiIB = 0.2
 
+ER_network <- function(network_size, prob){
+  a = matrix(0, network_size, network_size)
+  links = sample(c(1:network_size), size = network_size * network_size, replace = T)
+  links = matrix(links, ncol = network_size, byrow = T)
+  threshold = prob * network_size
+  for (i in 1:network_size){
+    for (j in 1:network_size){
+      if ( i != j){
+        if (links[i,j] < threshold){
+          a[i,j] = 1
+        }
+      }
+    }
+  }
+  G = graph_from_adjacency_matrix(a, mode = 'directed')
+  return (G)
+}
+
+# test
+# a = ER_network(100, 3/99)
+# cat(length(E(a))/100)
+
 create_network <- function(network_size, prob, print_out = FALSE){
   
   # first creat the graph without weights
-  G <- sample_gnp(n=network_size, p=prob, TRUE)
+  # G <- sample_gnp(n=network_size, p=prob, TRUE)
+  G <- ER_network(network_size, prob)
   M <- length(E(G))
   edge_df <- ends(G, E(G))
   if (print_out == TRUE) sprintf('The network have %s edges, hence the average degree is %s',
