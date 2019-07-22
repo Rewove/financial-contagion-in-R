3f62697122b23794ea7172f9d70597e16d630655
rewove <rewove@github.com>
2019-07-22 21:02:54 +0100

    add ER with target policy

---
+++ ER_tp.R
@@ -0,0 +1,70 @@
+#source('~/Desktop/network/create_network.R')
+#source('~/Desktop/network/judge_bankrupt.R')
+#source('~/Desktop/network/simulate_bankrupt.R')
+source('create_network.R')
+source('judge_bankrupt.R')
+source('simulate_bankrupt.R')
+
+network_size <- 1000
+simulation_times <- 100  # 100
+x_average_dgree <- seq(0, 10.1, 0.2)  # 0.2
+prob <- x_average_dgree/(network_size -1)
+contagion_threshould <- 0.05
+threshould <- network_size * contagion_threshould
+
+
+main <- function(){
+  y_prob <- c()
+  y_exte <- c()
+  for (j in prob) {
+    count_contagion <- 0
+    sum_percentages <- 0
+    cat('Doing simulation on Average Degree: ')
+    cat(j*(network_size-1))
+    cat('\n')
+    
+    for (i in 1:simulation_times){
+      if (i==100){
+        cat('No.')
+        cat(i)
+        cat('\n')
+      } else if (i %% 10 == 0) {
+        cat('No.')
+        cat(i)
+        cat('...')
+      }
+      G <- create_network(network_size, j, type = 'er')
+      r <- simulate_bankrupt(G, method = 'biggest', type = 'num', target_policy = TRUE)
+      r <- as.numeric(r)
+      
+      if (r > threshould){
+        count_contagion <- count_contagion +1
+        percentage_cont <- r/network_size
+        sum_percentages <- sum_percentages + percentage_cont
+      }
+    }
+    
+    proba_contagion <- count_contagion / simulation_times
+    if (count_contagion != 0){
+      exten_contagion <- sum_percentages / count_contagion
+    } else{
+      exten_contagion <- 0
+    }
+    y_prob <- cbind(y_prob, proba_contagion)
+    y_exte <- cbind(y_exte, exten_contagion)
+  }
+  
+  return(do.call(rbind, Map(data.frame, y_prob=y_prob, y_exte=y_exte)))
+
+}
+
+system.time({
+  results <- main()
+  cat('Saving the results ... ')
+  cat('\n')
+  write.table(results,file="results_er_target.csv", sep=',', quote=F,col.name=F,row.names=F)
+  plot_the_figure(p_cc, results$y_prob, results$y_exte, 
+                  network_name = 'ER Network',
+                  xlab = 'Average Degree (Connectivity)',
+                  notes = 'Random Bankrupt with Target Policy')
+})
